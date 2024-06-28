122ecd71c9d116f741466e6cfc69068dc7b96208
Peter Galonza <p.galonza@evaron.ru>
2024-06-28 23:46:17 +0300

    feature: use ptyhon3

--- emummc/build.sh
+++ emummc/build.sh
@@ -5,5 +5,5 @@ set -e
 make clean
 make -j
 ./hactool -t kip emummc.kip --uncompressed emummc_unpacked.kip
-python2.7 tools/kip1converter.py emummc_unpacked.kip emummc.data
+python3 tools/kip1converter.py emummc_unpacked.kip emummc.data
 cat emummc.caps emummc.data > emummc.kipm
