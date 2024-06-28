6c53f7d4baa58bf9394704c9b190c4fb88d8c32b
Peter Galonza <p.galonza@evaron.ru>
2024-06-28 23:45:20 +0300

    feature: nix hactool build

--- emummc/build.sh
+++ emummc/build.sh
@@ -4,6 +4,6 @@ set -e
 
 make clean
 make -j
-./hactool.exe -t kip emummc.kip --uncompressed emummc_unpacked.kip
+./hactool -t kip emummc.kip --uncompressed emummc_unpacked.kip
 python2.7 tools/kip1converter.py emummc_unpacked.kip emummc.data
 cat emummc.caps emummc.data > emummc.kipm
